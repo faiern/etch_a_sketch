5d8f1875c5496db33823d5d315b44ce623d505a3
faiern <thomasl.tlouis@gmail.com>
2023-01-21 21:33:41 +0100

    FINISHED git add .! ?????

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 
 const container = document.querySelector("#container");
+let numberOfDivs = 16;
 
-for( let i=0; i<16; i++){
-    const div = document.createElement("div");
-    div.textContent = "div number : " + i;
-    div.addEventListener("mouseover",()=>{
-        div.style.backgroundColor = "red";
-    
-    });
-    div.addEventListener("mouseleave",()=>{
-        div.style.backgroundColor = "green";
-    
-    });
-    container.appendChild(div);
+const boutton = document.querySelector("button");
+
+create_grid(8);
+
+boutton.addEventListener("click",()=>{
+    numberOfDivs = prompt("Combien de cases, bg ?");
+    create_grid(numberOfDivs);
+})
+
+function create_grid(numberOfDivs){
+    for( let i=0; i<numberOfDivs; i++){
+        const div = document.createElement("div");
+        div.textContent = "div number : " + i;
+        div.addEventListener("mouseover",()=>{
+            div.style.backgroundColor = "red";
+        
+        });
+        div.addEventListener("mouseleave",()=>{
+            div.style.backgroundColor = "green";
+        
+        });
+        container.appendChild(div);
+    }
 }
